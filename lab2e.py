298a85fa2c86513c37dc7984daae2e6a34fc6b36
Sabin Bhujel <sbhujel2@myseneca.ca>
2024-05-24 17:07:48 -0400

    Added files.

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Author : Sabin Bhujel
+#Author ID: 180147217
+#Date Created: 2024/05/24
+
+timer = 10 
+while timer > 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
+
